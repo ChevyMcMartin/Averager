ef77714136f67e68d0a453d054913da1596c7938
Chevy McMartin <ckmcmartin@gmail.com>
2018-10-23 19:17:27 -0400

    Finished it

--- Program.cs
+++ Program.cs
@@ -10,6 +10,47 @@ namespace Averager
     {
         static void Main(string[] args)
         {
+            double total = 0;
+            int count = 0;
+            
+            //Repeatedly prompt the user for numbers. 
+            while (true)
+            {
+                Console.Write("Enter a number or type \"done\" to see the average: ");
+                string entry = Console.ReadLine();
+                if (entry.ToLower() == "done")
+                {
+                    break;
+                }
+                try
+                {
+                    int number = int.Parse(entry);
+                    if(number <= 0)
+                    {
+                        Console.WriteLine("Number must be greater than 0");
+                        continue;
+                    }
+                    //Add all of the numbers together. 
+                    total += number;
+                    count += 1;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid entry.");
+                    continue;
+                }
+                catch(OverflowException)
+                {
+                    Console.WriteLine("Invalid entry.");
+                    continue;
+                }
+            }
+
+            //When the user types in “done”, 
+            //print the average of all of the numbers by dividing the total by the number of numbers entered.
+            double average = total / count;
+            Console.WriteLine("Average: " + average );
+            Console.ReadLine();
         }
     }
 }
